cfe22103b9c4d59fd6fc74ec12a20e66ef6d535b
David Jangraw <djangraw@users.noreply.github.com>
2021-06-08 17:15:55 -0400

    Renamed and cropped Arduino code

--- ArduinoCode/AnalogInToSerial_Time3Chan/AnalogInToSerial_Time3Chan.ino
+++ ArduinoCode/AnalogInToSerial_Time3Chan/AnalogInToSerial_Time3Chan.ino
@@ -4,21 +4,25 @@
   Reads 3 analog input pins, and prints the time and results to the Serial Monitor.
 
   Created 1/15/21 by DJ based on AnalogInToSerial.
+  Updated 3/6/21 by DJ - make sampling rate more constant
+  Updated 6/8/21 by DJ - renamed from AnalogInToSerial_Time3Chan_v2
 */
 
 // Declare Constants & Initialize Variables
 const int inputPins[] = {A0, A1, A2};           // Analog input pins
 unsigned long currentTime = 0;                  // time
+unsigned long nextTime = 0;                     // time to record sample
 int sensorValue = 0;                            // value read from the analog input pin
 int i = 0;                                      // counter
 const unsigned int fs = 500;                    // sampling rate in Hz (max should be 500)
-const unsigned int delayTimeMs = (1000/fs) - 1; // ms to wait between samples (reading/writing takes ~1ms)
+const unsigned int delayTimeMs = (1000/fs);     // ms to wait between samples
 
 
 // Run once on startup
 void setup() {
   // initialize serial communications at 500000 bps:
   Serial.begin(500000);
+  currentTime = millis(); 
 }
 
 
@@ -26,7 +30,9 @@ void setup() {
 void loop() {
 
   // get the current time
-  currentTime = millis(); 
+  while (currentTime<nextTime) {
+    currentTime = millis(); 
+  }
 
   // print the time to the Serial Monitor
   Serial.print(currentTime);
@@ -44,5 +50,5 @@ void loop() {
   Serial.println();
 
   // wait to keep the sampling rate constant:
-  delay(delayTimeMs);
+  nextTime = nextTime+delayTimeMs;
 }
